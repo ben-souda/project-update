4bc79499014f0ab413a753aa7092d852e7a0883f
ben-souda <76470837+ben-souda@users.noreply.github.com>
2021-03-24 08:40:22 +0100

    Add files via upload
    hi

---
+++ projet-3-javascript/js/inscription.js
@@ -0,0 +1,16 @@
+function google() {
+    var text=document.getElementById("Nom").value;
+    document.getElementById ("GG").innerHTML+="<b>Nom:</b>&nbsp;"+text+"<br>";
+        
+            var text=document.getElementById("Prénom").value;
+            document.getElementById ("GG").innerHTML+="<b>Prénome:</b>&nbsp;"+text+"<br>";
+                
+            var text=document.getElementById("Adresse").value;
+            document.getElementById ("GG").innerHTML+="<b>Adresse:</b>&nbsp;"+text+"<br>";
+                
+                    var text=document.getElementById("Téléphone").value;
+                    document.getElementById ("GG").innerHTML+="<b>Téléphone:</b>&nbsp;"+text+"<br>";
+                   
+                    var text=document.getElementById("Email").value;
+                    document.getElementById ("GG").innerHTML+="<b>Email:</b>&nbsp;"+text+"<br>";
+                }
